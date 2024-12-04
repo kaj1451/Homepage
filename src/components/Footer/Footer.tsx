49b8ce7e371c2557a5380085d7662cf231059924
kaj1451 <kajendran.suyambulingam@cdw.com>
2024-12-04 18:43:15 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <div className="text-[14px] text-gray-500 mt-[50px]">
+      &copy; {new Date().getFullYear()} CDW MetricVision. All rights reserved.
+    </div>
+  );
+}
