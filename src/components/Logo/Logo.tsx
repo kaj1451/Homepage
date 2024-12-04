8c438d47e57ffea483ced15770c08fab47be5e4c
kaj1451 <kajendran.suyambulingam@cdw.com>
2024-12-04 18:45:33 +0530

    Add files via upload

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import { Image } from '../ui/Image';
+
+export function Logo() {
+  return (
+    <Image
+      src="https://raw.githubusercontent.com/cdw/cdw-brand/main/cdw-2023-Red.png"
+      alt="CDW Logo"
+      className="w-[150px] mt-5"
+    />
+  );
+}
